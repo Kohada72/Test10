18fedadc0dfed19086731e5c354a1631e0b8936c
KazumaWada <blackpepper485@gmail.com>
2024-07-03 19:28:13 +0900

    環境整備

--- pymongo_update.py
+++
@@ -1,9 +0,0 @@
-from pymongo_get_database import get_database
-
-dbname = get_database()
-
-# Create a new collection
-
-collection_name = dbname["user_1_items"]
-#'_id'が'U1TT00001'
-collection_name.update_one({'_id':'U1IT00001'}, {"$set": { 'price' : 350}})

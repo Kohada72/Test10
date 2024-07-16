0887f871a525880a0ee0a70557f101a1b8136936
al22073 <al22073@shibaura-it.ac.jp>
2024-07-16 14:18:36 +0900

    correct error

--- UI_package/signupUI.py
+++ UI_package/signupUI.py
@@ -13,12 +13,22 @@ def signupUI1():
     return render_template('signup.html')
 
 def signupUI2(user_id, user_name, user_passward):
+    
+    #ユーザIDのフォーム判定
     try:
         user_id = int(user_id)
     except ValueError:
         flash("ユーザIDの欄には数値を入力して下さい")
         return redirect("/signup")
     
+    #ユーザIDの長さ判定
+    if len(str(user_id)) >= 9:
+        flash("ユーザIDの長さは8桁以内です")
+        return redirect("/signup")
+    elif len(user_passward) >= 17:
+        flash("パスワードは16文字以内です")
+        return redirect("/signup")
+        
     if(addUser(user_id, user_name, user_passward)):
         flash("ユーザ登録が完了しました")
         return redirect("/login")
